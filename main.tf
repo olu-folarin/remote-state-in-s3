2734c741a2f757365b1962ccfc9734179f23eef5
olufolarin <folami25@gmail.com>
2022-02-22 15:28:17 +0100

    inserting the s3 bucket created in the backend-state folder

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+// insert the s3 bucket config here
+terraform {
+  // name of the backend -- s3
+  backend "s3" {
+    bucket = 
+    key = 
+    region = 
+    dynamodb_table = 
+    encryption = 
+  }
+}
+
 terraform {
   required_providers {
     aws = {
