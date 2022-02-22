59dfdd7d4b4934647424423e796d67e10e756956
olufolarin <folami25@gmail.com>
2022-02-22 15:44:20 +0100

    created varaibles for the config

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   // name of the backend -- s3
   backend "s3" {
     bucket = "dev-applications-backend-state-olufolarin"
-    key = "backend-tf-rs-s3-backend-dev"
+    key = "${var.application_name}-${var.project_name}-${var.project_environment}"
     region = "us-east-1"
     dynamodb_table = "dev_application_locks"
     encryption = 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable application_name [
+  default = "backend"
+]
+
+variable project_name {
+  default = "tf-r3-s3-backend"
+}
+
+variable project_environment {
+  default = "dev"
+}
